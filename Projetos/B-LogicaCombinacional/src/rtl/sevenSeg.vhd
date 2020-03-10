4ebba5944192bb6c4460356f0e41a529d5cfee5b
Joao Zsigmond <jzsiggy@gmail.com>
2020-03-10 11:31:00 -0300

    Adding toplevel.vhd and modifying sevenseg

--- Projetos/B-LogicaCombinacional/src/rtl/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/rtl/sevenSeg.vhd
@@ -3,22 +3,65 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sevenSeg is
 	port (
-			bcd : in  STD_LOGIC_VECTOR(3 downto 0);
-			leds: out STD_LOGIC_VECTOR(6 downto 0));
-end entity;
+		bcd0, bcd1, bcd2, bcd3: in  std_logic_vector(3 downto 0);
+		led0, led1, led2, led3: out std_logic_vector(6 downto 0)
+	);	
+	end entity;
 
 architecture arch of sevenSeg is
-
 begin
-		leds <= "0000001" when (bcd = "0000") else
-			"0110000" when (bcd = "0001") else
-			"1101101" when (bcd = "0010") else
-			"0000110" when (bcd = "0011") else
-			"0110011" when (bcd = "0100") else
-			"1011011" when (bcd = "0101") else
-			"1011111" when (bcd = "0110") else
-			"0001111" when (bcd = "0111") else
-			"1111111" when (bcd = "1000") else
-			"1111011" when (bcd = "1001");
 
+	with bcd0 select
+
+	led0(6 downto 0) <= "1000000" when "0000",
+				"1111001" when "0001",
+				"0100100" when "0010",
+				"0110000" when "0011",
+				"0011001" when "0100",
+				"0010010" when "0101",
+				"0000010" when "0110",
+				"1111000" when "0111",
+				"0000000" when "1000",
+				"0011000" when "1001",
+				"1111111" when others;
+	
+	with bcd1 select
+	
+
+	led1(6 downto 0) <= "1000000" when "0000",
+				"1111001" when "0001",
+				"0100100" when "0010",
+				"0110000" when "0011",
+				"0011001" when "0100",
+				"0010010" when "0101",
+				"0000010" when "0110",
+				"1111000" when "0111",
+				"0000000" when "1000",
+				"0011000" when "1001",
+				"1111111" when others;
+	with bcd2 select
+	led2(6 downto 0) <= "1000000" when "0000",
+				"1111001" when "0001",
+				"0100100" when "0010",
+				"0110000" when "0011",
+				"0011001" when "0100",
+				"0010010" when "0101",
+				"0000010" when "0110",
+				"1111000" when "0111",
+				"0000000" when "1000",
+				"0011000" when "1001",
+				"1111111" when others;
+	with bcd3 select
+	led3(6 downto 0) <= "1000000" when "0000",
+				"1111001" when "0001",
+				"0100100" when "0010",
+				"0110000" when "0011",
+				"0011001" when "0100",
+				"0010010" when "0101",
+				"0000010" when "0110",
+				"1111000" when "0111",
+				"0000000" when "1000",
+				"0011000" when "1001",
+				"1111111" when others;
+				
 end architecture;
