ff786c78a341bd48f69a865a0d1fa8325cf0fd5d
arthurfc12 <arthur0912fc@gmail.com>
2020-04-01 15:41:46 -0300

    flipflop

--- Projetos/D-LogicaSequencial/src/TopLevel.vhd
+++ Projetos/D-LogicaSequencial/src/TopLevel.vhd
@@ -1,69 +1,32 @@
---
--- Elementos de Sistemas - Aula 5 - Logica Combinacional
--- Rafael . Corsi @ insper . edu . br 
---
--- Arquivo exemplo para acionar os LEDs e ler os bottoes
--- da placa DE0-CV utilizada no curso de elementos de 
--- sistemas do 3s da eng. da computacao
+-- Elementos de Sistemas
+-- by Luciano Soares
+-- FlipFlopD.vhd
 
-----------------------------
--- Bibliotecas ieee       --
-----------------------------
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use work.all;
 
-----------------------------
--- Entrada e saidas do bloco
-----------------------------
 entity TopLevel is
-	port(
-		SW      : in  std_logic_vector(9 downto 0);
-		KEY     : in  std_logic_vector(3 downto 0);
-		LEDR    : out std_logic_vector(9 downto 0)
-	);
-end entity;
-
-----------------------------
--- Implementacao do bloco -- 
-----------------------------
-architecture rtl of TopLevel is
-
-
-component FlipFlopD is
 	port(
 		clock:  in std_logic;
 		d:      in std_logic;
 		clear:  in std_logic;
 		preset: in std_logic;
-		q:     out std_logic
+		q:      out std_logic := '0'
 	);
-end component;
+end entity;
 
---------------
--- signals
---------------
+architecture arch of TopLevel is
 
-signal clock, clear, set : std_logic;
-
----------------
--- implementacao
----------------
 begin
 
-Clock <= not KEY(0); -- os botoes quando nao apertado vale 1
-                     -- e apertado 0, essa logica inverte isso
-clear <= not KEY(1);
-set	<= not KEY(2);
+	process(clock, clear) begin
+		if (clear = '1') then
+			q <='0';
+		elsif (preset = '1') then
+			q <= '1';
+		elsif (rising_edge(CLOCK)) then
+			q<=D;
+		end if;
+  end process;
 
-u0 : FlipFlopD port map (
-		clock    => Clock,
-		d        => SW(0),
-		clear    => clear,
-		preset   => set,
-		q        => LEDR(0)
-	);		
- 
-
-end rtl;
+end architecture;
