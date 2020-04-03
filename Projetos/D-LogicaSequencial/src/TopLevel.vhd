57b5f59e97c56650113b3e34ec01a56feea86348
arthurfc12 <arthur0912fc@gmail.com>
2020-04-03 01:23:49 -0300

    opa

--- Projetos/D-LogicaSequencial/src/TopLevel.vhd
+++ Projetos/D-LogicaSequencial/src/TopLevel.vhd
@@ -1,32 +1,45 @@
 -- Elementos de Sistemas
 -- by Luciano Soares
--- FlipFlopD.vhd
+-- BinaryDigit.vhd
 
-library ieee;
+Library ieee;
 use ieee.std_logic_1164.all;
 
 entity TopLevel is
 	port(
-		clock:  in std_logic;
-		d:      in std_logic;
-		clear:  in std_logic;
-		preset: in std_logic;
-		q:      out std_logic := '0'
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC;
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC
 	);
 end entity;
 
 architecture arch of TopLevel is
 
-begin
+	component FlipFlopD is
+		port(
+			clock:  in std_logic;
+			d:      in std_logic;
+			clear:  in std_logic;
+			preset: in std_logic;
+			q:     out std_logic
+		);
+	end component;
 
-	process(clock, clear) begin
-		if (clear = '1') then
-			q <='0';
-		elsif (preset = '1') then
-			q <= '1';
-		elsif (rising_edge(CLOCK)) then
-			q<=D;
-		end if;
-  end process;
+	component Mux2Way is
+		port (
+			a:   in  STD_LOGIC;
+			b:   in  STD_LOGIC;
+			sel: in  STD_LOGIC;
+			q:   out STD_LOGIC);
+	end component;
+
+	signal dffout,muxout: std_logic;
+
+begin
+	
+	P1: Mux2Way port map (dffout,input,load,muxout);
+	P2: FlipFlopD port map(clock,muxout,'0','0', dffout);
+	output <= dffout; 
 
 end architecture;
