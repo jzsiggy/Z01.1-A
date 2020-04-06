7c3e51b2ff768cc536e57c15c02b155983a7eee1
Bilbia <biamcs2000@gmail.com>
2020-04-06 12:31:50 -0300

    ram4k e forma de onda feitos

--- Projetos/D-LogicaSequencial/src/rtl/Ram4K.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/Ram4K.vhd
@@ -59,6 +59,16 @@ architecture arch of Ram4K is
 	signal output0, output1, output2, output3, output4, output5, output6, output7 : STD_LOGIC_VECTOR(15 downto 0);
 
 begin
+	REGISTRA1: Ram512 port map(clock,input,load0, address(8 downto 0), output0);
+	REGISTRA2: Ram512 port map(clock,input,load1, address(8 downto 0), output1);
+	REGISTRA3: Ram512 port map(clock,input,load2, address(8 downto 0), output2);
+	REGISTRA4: Ram512 port map(clock,input,load3, address(8 downto 0), output3);
+	REGISTRA5: Ram512 port map(clock,input,load4, address(8 downto 0), output4);
+	REGISTRA6: Ram512 port map(clock,input,load5, address(8 downto 0), output5);
+	REGISTRA7: Ram512 port map(clock,input,load6, address(8 downto 0), output6);
+	REGISTRA8: Ram512 port map(clock,input,load7, address(8 downto 0), output7);
+	MUX: Mux8Way16 port map( output0,output1,output2,output3,output4,output5,output6,output7,address(11 downto 9),output);
+	DMUX: DMux8Way port map( load,address(11 downto 9),load0,load1,load2,load3,load4,load5,load6,load7);  
 
 
 end architecture;
