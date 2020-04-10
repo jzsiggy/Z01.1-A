b0d6ad22bb098e863dc0451df9ba0a61b659983e
arthurfc12 <arthur0912fc@gmail.com>
2020-04-09 21:33:17 -0300

    tpdps os rtls

--- Projetos/D-LogicaSequencial/tests/tst/tb_CounterDown.vhd
+++ Projetos/D-LogicaSequencial/tests/tst/tb_CounterDown.vhd
@@ -34,11 +34,37 @@ begin
     test_runner_setup(runner, runner_cfg);
 
     -- IMPLEMENTE AQUI!
-    outQ <= "111";
+    q <= "111";
     wait until clk'event and clk='0';
-		assert(outQ = "111")  report "Falha em teste: 0" severity error;
+		assert(q = "110")  report "Falha em teste: 0" severity error;
 
+    q <= "110";
+    wait until clk'event and clk='0';
+		assert(q = "101")  report "Falha em teste: 0" severity error;
 
+    q <= "101";
+    wait until clk'event and clk='0';
+		assert(q = "100")  report "Falha em teste: 0" severity error;
+
+    q <= "100";
+    wait until clk'event and clk='0';
+		assert(q = "011")  report "Falha em teste: 0" severity error;
+
+    q <= "011";
+    wait until clk'event and clk='0';
+		assert(q = "010")  report "Falha em teste: 0" severity error;
+
+    q <= "010";
+    wait until clk'event and clk='0';
+		assert(q = "001")  report "Falha em teste: 0" severity error;
+
+    q <= "001";
+    wait until clk'event and clk='0';
+		assert(q = "000")  report "Falha em teste: 0" severity error;
+
+    q <= "000";
+    wait until clk'event and clk='0';
+		assert(q = "111")  report "Falha em teste: 0" severity error;
 
     -- finish
     wait until clk'event and clk='0';
