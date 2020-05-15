cf7c74240bdf539a0983969ab195716ee23a9712
arthurfc12 <arthur0912fc@gmail.com>
2020-05-15 08:18:27 -0300

    atualizando pro prof

--- Projetos/C-UnidadeLogicaAritmetica/src/rtl/Add16.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/rtl/Add16.vhd
@@ -12,109 +12,38 @@ entity Add16 is
 	port(
 		a   :  in STD_LOGIC_VECTOR(15 downto 0);
 		b   :  in STD_LOGIC_VECTOR(15 downto 0);
-		q   : out STD_LOGIC_VECTOR(15 downto 0);
-		carry : out std_logic
+		q   : out STD_LOGIC_VECTOR(15 downto 0)
 	);
 end entity;
 
 architecture rtl of Add16 is
-  -- Aqui declaramos sinais (fios auxiliares)
-  -- e componentes (outros módulos) que serao
-  -- utilizados nesse modulo.
-  
-  signal c : STD_LOGIC_VECTOR(15 downto 0);
 
-  component FullAdder is
-    port(
-      a,b,c:      in STD_LOGIC;   -- entradas
-      soma,vaium: out STD_LOGIC   -- sum e carry
-    );
-  end component;
+component FullAdder is
+	port(
+		a,b,c:      in STD_LOGIC;   -- entradas
+		soma,vaium: out STD_LOGIC   -- sum e carry
+	);
+end component;
 
+signal s: std_logic_vector(14 downto 0);
 
 begin
 
-	c(0) <= '0';
+	u0  : FullAdder port map ( a(0), b(0),  '0', q(0), s(0));
+	u1  : FullAdder port map ( a(1), b(1), s(0), q(1), s(1));
+	u2  : FullAdder port map ( a(2), b(2), s(1), q(2), s(2));
+	u3  : FullAdder port map ( a(3), b(3), s(2), q(3), s(3));
+	u4  : FullAdder port map ( a(4), b(4), s(3), q(4), s(4));
+	u5  : FullAdder port map ( a(5), b(5), s(4), q(5), s(5));
+	u6  : FullAdder port map ( a(6), b(6), s(5), q(6), s(6));
+	u7  : FullAdder port map ( a(7), b(7), s(6), q(7), s(7));
+	u8  : FullAdder port map ( a(8), b(8), s(7), q(8), s(8));
+	u9  : FullAdder port map ( a(9), b(9), s(8), q(9), s(9));
+	u10 : FullAdder port map (a(10),b(10), s(9),q(10),s(10));
+	u11 : FullAdder port map (a(11),b(11),s(10),q(11),s(11));
+	u12 : FullAdder port map (a(12),b(12),s(11),q(12),s(12));
+	u13 : FullAdder port map (a(13),b(13),s(12),q(13),s(13));
+	u14 : FullAdder port map (a(14),b(14),s(13),q(14),s(14));
+	u15 : FullAdder port map (a(15),b(15),s(14),q(15),open);
 	
-  u0 : FullAdder port map(a => a(0),
-									 b => b(0),
-									 c => c(0),
-									 soma => q(0),
-									 vaium => c(1));
-  u1 : FullAdder port map(a => a(1),
-									 b => b(1),
-									 c => c(1),
-									 soma => q(1),
-									 vaium => c(2));
-  u2 : FullAdder port map(a => a(2),
-									 b => b(2),
-									 c => c(2),
-									 soma => q(2),
-									 vaium => c(3));
-  u3 : FullAdder port map(a => a(3),
-									 b => b(3),
-									 c => c(3),
-									 soma => q(3),
-									 vaium => c(4));					
-  u4 : FullAdder port map(a => a(4),
-									 b => b(4),
-									 c => c(4),
-									 soma => q(4),
-									 vaium => c(5));					
-  u5 : FullAdder port map(a => a(5),
-									 b => b(5),
-									 c => c(5),
-									 soma => q(5),
-									 vaium => c(6));
-  u6 : FullAdder port map(a => a(6),
-									 b => b(6),
-									 c => c(6),
-									 soma => q(6),
-									 vaium => c(7));
-  u7 : FullAdder port map(a => a(7),
-									 b => b(7),
-									 c => c(7),
-									 soma => q(7),
-									 vaium => c(8));
-  u8 : FullAdder port map(a => a(8),
-									 b => b(8),
-									 c => c(8),
-									 soma => q(8),
-									 vaium => c(9));
-  u9 : FullAdder port map(a => a(9),
-									 b => b(9),
-									 c => c(9),
-									 soma => q(9),
-									 vaium => c(10));
-  u10 : FullAdder port map(a => a(10),
-									 b => b(10),
-									 c => c(10),
-									 soma => q(10),
-									 vaium => c(11));					
-  u11 : FullAdder port map(a => a(11),
-									 b => b(11),
-									 c => c(11),
-									 soma => q(11),
-									 vaium => c(12));  
-  u12 : FullAdder port map(a => a(12),
-									 b => b(12),
-									 c => c(12),
-									 soma => q(12),
-									 vaium => c(13));
-  u13 : FullAdder port map(a => a(13),
-									 b => b(13),
-									 c => c(13),
-									 soma => q(13),
-									 vaium => c(14));								
-  u14 : FullAdder port map(a => a(14),
-									 b => b(14),
-									 c => c(14),
-									 soma => q(14),
-									 vaium => c(15));										
-  u15 : FullAdder port map(a => a(15),
-									 b => b(15),
-									 c => c(15),
-									 soma => q(15),
-									 vaium => carry);	
-
 end architecture;
--- Projetos/F-Computador/src/rtl/CPU.vhd
+++ Projetos/F-Computador/src/rtl/CPU.vhd
@@ -100,24 +100,23 @@ architecture arch of CPU is
 
 begin
 
-  pc: PC port map (clock, inc, c_loadPC, reset, input, s_pcout(15 downto 0));
+  ProgramCounter: PC port map (clock, '1', c_loadPC, reset, s_regAout, s_pcout(15 downto 0)); --
   
-  muxalui: Mux16 port map (a,b,sel, s_muxALUI_Aout);
+  MuxAlui: Mux16 port map (s_ALUout, instruction(15 downto 0), c_muxALUI_A, s_muxALUI_Aout); --
   
-  muxam: Mux16 port map (a,b,sel, s_muxAM_out);
+  MuxAm: Mux16 port map (s_regAout, inM, c_muxAM, s_muxAM_out); --
   
-  regA: Register16 port map(clock, input, load, s_regAout);
+  RegisterA: Register16 port map(clock, s_muxALUI_Aout, c_loadA, s_regAout); --
   
-  regD: Register16 port map(clock, s_ALUout, load, s_regDout);
+  RegisterD: Register16 port map(clock, s_ALUout, c_loadD, s_regDout); --
 
-  ula: ALU port map(s_regDout, s_muxAM_out, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_zr, c_ng, s_ALUout);
+  ULA: ALU port map(s_regDout, s_muxAM_out, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_zr, c_ng, s_ALUout); --
 
-  controlUnit: ControlUnit port map(instruction, c_zr, c_ng, c_muxALUI_A, c_muxAM, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_loadA, c_loadD, loadM, c_loadPC);
+  Control: ControlUnit port map(instruction, c_zr, c_ng, c_muxALUI_A, c_muxAM, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_loadA, c_loadD, writeM, c_loadPC); --
 
 
-  outM <= 
-  addressM <= 
-  writeM <= 
-  pcout <= 
+  outM <= s_ALUout;
+  addressM <= s_regAout(14 downto 0); 
+  pcout <= s_pcout(14 downto 0);
 
 end architecture;
